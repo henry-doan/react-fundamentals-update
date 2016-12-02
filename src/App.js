c3aa72891b9ef1c96ac1c5d98f9d5603664b3720
Henry Doan <henrydoan96@gmail.com>
2016-12-02 14:04:39 -0700

    managing states with def, and notes in the comments

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import React from 'react';
 
 class App extends React.Component {
-  render(){
-    /*
-      to access props
-      - by interpolating
-      - with this.props and the name of the prop
+  constructor(){
+    super();
+    /* 
+      super will give use the key word this
+      the context within our component rather 
+      than it's current class.
     */
-    let txt = this.props.txt
-    return <h1>{txt}</h1>
+
+    /* 
+      the call of this.setState only overide the
+      values we actually send in to it.
+    */
+
+    this.state = {
+      txt: 'this is the state text',
+      cat: 0
+    }
+    // this is managed by our component
+
+  }
+
+  // this is a custom method
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+
+  render(){
+    return (
+      <div>
+        <input 
+          type=""
+          onChange={this.update.bind(this)}
+        />
+        <h1>{this.state.txt} - {this.state.cat}</h1>
+      </div>
+    )
   }
 }
 
 /*
-  we can defined the properties we are going to be looking for
-  in our component by adding a property to our component called 
-  PropTypes
+  props
+    - are a collections of values that are meant
+      to be passed in to our component at static
+      values not to be changed by our component
 
-  this is going to be an object which keys and values
-  the keys is the name of the property
-  and the values is the type of the value we are looking for.
-
+  state
+    - represent a collection of values that is 
+      meant to be managed as well as updated by
+      the component(s)
 */
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-/*
-  set default proptypes
-
-*/
-
-App.defaultProps = {
-  txt: "this is the default props"
-}
 
 export default App
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-	//passing in a prop
-	//the value over writes the default
-	//removing it wil have the default value
-  <App cat={3} txt="this is a prop txt" />,
+  <App />,
   document.getElementById('root')
 );

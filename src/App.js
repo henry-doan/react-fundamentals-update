deb99325d75497bccb1b483d843a7c9401d4fbe4
Henry Doan <henrydoan96@gmail.com>
2016-12-02 13:23:47 -0700

    render method with comment notes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <h1>Hello World!</h1>
-    // This makes this
-    // React.createElement('h1', null, 'Hello World')
-    
-    // the first is the html element
-    // all custom react component needs to start with a capitol letter
-    // lower case is a native element.
-    // second arg is for props
-    // the third arg can be another elment or a string for the inner html.   
+    // the right way to render something
+    // best practice way
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <b>Bold</b>
+      </div>
+    )
+
+    /* 
+      the reason why just this doesn't work
+      
+        return <h1>Hello world!</h1> <br/>
+
+      is because its is trying to do:
+
+        return React.createElement('', , ) React.createElement('', , )
+      one after the other, which is invaid.
+
+    */
+
+    /*
+      another way to write the above and works just fine is:
+
+      return <div>
+          <h1>Hello World!</h1>
+          <b>Bold</b>
+        </div>
+
+    */
   }
 }
 
-// stateless function component
-// another way to make a component
-// does not have state.
-// const App = () => <h1>Hello world stateless</h1>
 export default App
 

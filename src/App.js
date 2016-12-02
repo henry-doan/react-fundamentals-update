5728cd17ed9aa2a89de4fad1450b5ef6e3efa6d5
Henry Doan <henrydoan96@gmail.com>
2016-12-02 15:02:09 -0700

    use react components as children for other components

--- src/App.js
+++ src/App.js
@@ -3,26 +3,11 @@ import React from 'react';
 class App extends React.Component {
   constructor(){
     super();
-    /* 
-      super will give use the key word this
-      the context within our component rather 
-      than it's current class.
-    */
-
-    /* 
-      the call of this.setState only overide the
-      values we actually send in to it.
-    */
-
     this.state = {
-      txt: 'this is the state text',
-      cat: 0
+      txt: 'this is the state text'
     }
-    // this is managed by our component
-
   }
 
-  // this is a custom method
   update(e) {
     this.setState({txt: e.target.value})
   }
@@ -30,26 +15,18 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <input 
-          type=""
-          onChange={this.update.bind(this)}
-        />
-        <h1>{this.state.txt} - {this.state.cat}</h1>
+        <h1>{this.state.txt}</h1>
+        <Widget update={this.update.bind(this)}/>
       </div>
     )
   }
 }
 
+const Widget = (props) => 
+  <input type="text" onChange={props.update}/>
 /*
-  props
-    - are a collections of values that are meant
-      to be passed in to our component at static
-      values not to be changed by our component
-
-  state
-    - represent a collection of values that is 
-      meant to be managed as well as updated by
-      the component(s)
+  react components can render other react 
+  components
 */
 
 export default App

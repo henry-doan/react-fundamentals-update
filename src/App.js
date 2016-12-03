6b71c78c0fa982e1a55c8884071522076a3bd2c5
Henry Doan <henrydoan96@gmail.com>
2016-12-02 20:29:47 -0700

    reacts synthetic event system

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {currentEvent: '---'}
+    this.update = this.update.bind(this)
+  }
+
+  update(e){
+    this.setState({currentEvent: e.type})
+  }
+
   render(){
-    return <Title text="The Text" />
+    return (
+      <div>
+        <textarea 
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          onDoubleClick={this.update}
+          onTouchStart={this.update}
+          onTouchMove={this.update}
+          onTouchEnd={this.update}
+          cols="30"  
+          rows="10"/>
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+    )
   }
 }
 
-const Title = (props) => <h1>Title: {props.text}</h1>
-
-Title.propTypes = {
-  // custom validations
-  text(props, propName, component){
-    // prop name is not provided
-    if(!(propName in props)){
-      return new Error(`missing ${propName}`)
-    }
-
-    // the length
-    if(props[propName.length] < 6){
-      return new Error(`${propName} was too short`)
-    }
-  }
-}
 
 export default App
 

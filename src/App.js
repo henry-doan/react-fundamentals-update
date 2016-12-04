f1a2205ff85d039d8236a4b4d3c3457162f992d7
Henry Doan <henrydoan96@gmail.com>
2016-12-04 10:49:02 -0700

    React few lifecycle methods with notes in the comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(){
@@ -12,9 +13,52 @@ class App extends React.Component {
     this.setState({val: this.state.val + 1})
   }
 
+   /*
+    The order below corresponds to the order they fire in 
+    the DOM
+
+  */
+
+  /* 
+    ComponentWillMount
+      - This is going to fire off right before it's
+      is mounted on the DOM.
+
+      - This insures that it will make it into the DOM
+
+      - The componentWillMount only fire once and will
+      ever fire once.
+  */
+
+  componentWillMount(){
+    console.log('componentWillMount')
+  } 
+
+
   render(){
     console.log('render');
     return <button onClick={this.update}>{this.state.val}</button>
+  }
+
+  /*
+    ComponentDidMount
+      - This is going to fire odd once our component has
+      actually been mounted to the DOM
+
+  */
+
+  componentDidMount() {
+    console.log('componentDidMount')
+  }
+
+  /*
+    ComponentWillUnmount
+      - This will fire when the component is about to leave
+      the DOM
+  */
+
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
   }
 }
 
@@ -24,11 +68,34 @@ class App extends React.Component {
   UnMounting
     - When a component is removed from the DOM
 
-  There are a handfull of lifecycle methods we can
+  There are a hand full of life-cycle methods we can
   access at various stages of the state
 */
 
+/*
+  This component below will toggle between mount and unMount.
+*/
+
+class Wrapper extends React.Component {
+  mount(){
+    ReactDOM.render(<App />, document.getElementById('a'))
+  }
+
+  unmount(){
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'))
+  }
+
+  render(){
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>UnMount</button>
+        <div id="a"></div>
+      </div>
+    )
+  }
+}
 
 
-export default App
+export default Wrapper
 

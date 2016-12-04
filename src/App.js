238433db61e07a7dd4cc68c995e3bcaac18c8e54
Henry Doan <henrydoan96@gmail.com>
2016-12-04 11:01:37 -0700

    Manage state with LifeCycle methods, with notes in the comments

--- src/App.js
+++ src/App.js
@@ -13,52 +13,45 @@ class App extends React.Component {
     this.setState({val: this.state.val + 1})
   }
 
-   /*
-    The order below corresponds to the order they fire in 
-    the DOM
-
-  */
-
   /* 
-    ComponentWillMount
-      - This is going to fire off right before it's
-      is mounted on the DOM.
-
-      - This insures that it will make it into the DOM
-
-      - The componentWillMount only fire once and will
-      ever fire once.
+    This allows us to set the state or intercept the
+    state before it actually rendered.
   */
 
   componentWillMount(){
     console.log('componentWillMount')
+    this.setState({m: 2})
   } 
 
 
   render(){
     console.log('render');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>
+      {this.state.val * this.state.m}
+    </button>
   }
 
   /*
-    ComponentDidMount
-      - This is going to fire odd once our component has
-      actually been mounted to the DOM
+    We have access to our component in the DOM
+    When it is mounted, it shows the node,
+    and incrament the interval
 
   */
 
   componentDidMount() {
     console.log('componentDidMount')
+    console.log(ReactDOM.findDOMNode(this))
+    this.inc = setInterval(this.update, 500)
   }
 
   /*
-    ComponentWillUnmount
-      - This will fire when the component is about to leave
-      the DOM
+    This will stop the incrementation when the
+    component is unMounted.
   */
 
   componentWillUnmount(){
     console.log('componentWillUnmount')
+    clearInterval(this.inc)
   }
 }
 

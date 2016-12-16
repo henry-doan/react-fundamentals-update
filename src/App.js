694eac96a6aa11a9f5527dd6e71ad0660307496f
Henry Doan <henrydoan96@gmail.com>
2016-12-15 22:49:12 -0700

    Button and label starter code

--- src/App.js
+++ src/App.js
@@ -2,43 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {items: []}
-  }
-
-  componentWillMount(){
-    fetch( 'http://swapi.co/api/people/?format=json' )
-      .then( response => response.json() )
-      .then( ({results: items}) => this.setState({items}))
-  }
-
-  filter(e){
-    this.setState({filter: e.target.value})
-  }
-
   render(){
-    let items = this.state.items
-    if(this.state.filter){
-      items = items.filter( item =>
-        item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-    }
-      // at this point we get an error of each child in an array should have a unique key prop.
-      // need to add a key for the error
     return (
       <div>
-        <input type="text" onChange={this.filter.bind(this)}/>
-        {items.map(item => 
-          <Person key={item.name} person={item} />)}
-      
+       <Button>button</Button>
+       <hr/>
+       <Label>label</Label>
       </div>
     )
   }
 
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+const Button = (props) => <button>{props.children}</button>
 
+class Label extends React.Component {
+  render(){
+    return (
+      <label>{this.props.children}</label>
+    )
+  }
+}
 
 export default App
 

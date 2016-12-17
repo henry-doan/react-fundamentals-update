835011916b394b5bc1de9ad32cfe6da4c5a40f4d
Henry Doan <henrydoan96@gmail.com>
2016-12-16 18:39:10 -0700

    children utilities

--- src/App.js
+++ src/App.js
@@ -2,46 +2,32 @@ import React from 'react';
 import './style.css'
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '/* add your jsx here */',
-      output: '',
-      err: ''
-    }
-  }
-
-  update(e) {
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel.transform(code, { presets: ['es2015', 'react']}).code,
-        err: ''
-      })
-    }
-    catch(err){
-      this.setState({err: err.message})
-    }
-  }
-
-  componentWillMount(){
-    console.log('will Mount')
-  }
-
   render(){
     return (
-      <div>
-       <header>{this.state.err}</header>
-       <div className="container">
-        <textarea 
-        onChange={this.update.bind(this)}
-        defaultValue={this.state.input}/>
-        <pre>
-          {this.state.output}
-        </pre>
-       </div>
-      </div>
+      <Parent>
+        <div className="childA"></div>
+        <div className="childB"></div>
+      </Parent>
     )
+  }
+}
+
+class Parent extends React.Component {
+  render() {
+    {/* 
+      let items = React.Children.map(this.props.children, child => child)
+        works the same as the code below
+      let items = React.Children.toArray(this.props.children)
+    
+      For each method
+        let items = React.Children.forEach(this.props.children, child => console.log(child.props.className))
+
+    */}
+
+    // only needs one child
+    let items = React.Children.only(this.props.children)
+    console.log(items)
+    return null
   }
 }
 

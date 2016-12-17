40910fa47f1a511bc86b1ed4646b702a01c0850c
Henry Doan <henrydoan96@gmail.com>
2016-12-16 18:53:53 -0700

    react clone element for children components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import './style.css'
 
 class App extends React.Component {
   render(){
     return (
-      <Parent>
-        <div className="childA"></div>
-        <div className="childB"></div>
-      </Parent>
+      <Buttons>
+        <button value="A">A</button>        
+        <button value="B">B</button>
+        <button value="C">C</button>        
+      </Buttons>
     )
   }
 }
 
-class Parent extends React.Component {
+class Buttons extends React.Component {
+  constructor(){
+    super();
+    this.state = {selected: 'None'}
+  }
+
+  selectItem(selected){
+    this.setState({selected})
+  }
+
   render() {
-    {/* 
-      let items = React.Children.map(this.props.children, child => child)
-        works the same as the code below
-      let items = React.Children.toArray(this.props.children)
-    
-      For each method
-        let items = React.Children.forEach(this.props.children, child => console.log(child.props.className))
-
-    */}
-
-    // only needs one child
-    let items = React.Children.only(this.props.children)
-    console.log(items)
-    return null
+    let fn = child =>
+      React.cloneElement(child, {
+        onClick: this.selectItem.bind(this, child.props.value)
+      })
+    let items = React.Children.map(this.props.children, fn);
+    return (
+      <div>
+        <h2>You have Selected: {this.state.selected}</h2>
+        {items}
+      </div>
+    )
   }
 }
 
